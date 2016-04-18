aa5e1c32995f4f779470b1f28e60eaffa170a8e0
langhaarzombie <mixmax199989@googlemail.com>
2016-04-18 15:54:22 +0200

    Added: model for MainWindow; Fixed: DataSet - works now, connection to school database; Deleted: old dataset

---
+++ program/Garlic_Client/Garlic_Client/models/mw_model.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+
+
+namespace Garlic_Client.models {
+    class mw_model : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        GarlicDatabaseEntities db = new GarlicDatabaseEntities();
+
+        public IEnumerable<c_cloves> AllCloves {
+            get {
+                return (from c in db.c_cloves
+                        orderby c.c_id, c.c_name
+                        select c).ToList();
+            }
+        }
+
+        private int selectedClove;
+        public int SelectedClove {
+            get {
+                return this.selectedClove;
+            }
+            set {
+                selectedClove = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("SelectedCloveDescription"));
+            }
+        }
+
+        public string SelectedCloveDescription {
+            get {
+                return (from c in db.c_cloves
+                        where c.c_id == this.selectedClove
+                        select c.c_description).First().ToString();
+                ;
+            }
+        }
+
+    }
+}
