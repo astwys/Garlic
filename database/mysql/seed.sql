8e7a928d20ae42840d9351591cd466d8cf02fd7d
astwys <astwys@gmail.com>
2016-04-19 10:16:32 +0200

    renamed insert.sql to seed.sql & added delete from's

--- database/mysql/seed.sql
+++ database/mysql/seed.sql
@@ -7,14 +7,16 @@ use garlic;
 
 set foreign_key_checks = 0;
 
-#u_users	done
+#u_users
+delete from u_users;
 load data infile '/Users/michael/school/3EHIF/DBI/Garlic/database/mysql/data/u_users.csv'
 into table u_users
 fields terminated by ','
 lines terminated by '\n'
 ignore 1 rows;
 
-#p_posts	done
+#p_posts
+delete from p_posts;
 load data infile '/Users/michael/school/3EHIF/DBI/Garlic/database/mysql/data/p_posts.csv'
 into table p_posts
 fields terminated by ','
@@ -22,7 +24,8 @@ optionally enclosed by '"'
 lines terminated by '\n'
 ignore 1 rows;
 
-#c_clove	done
+#c_clove
+delete from c_clove;
 load data infile '/Users/michael/school/3EHIF/DBI/Garlic/database/mysql/data/c_cloves.csv'
 into table c_clove
 fields terminated by ','
@@ -31,13 +34,15 @@ lines terminated by '\n'
 ignore 1 rows;
 
 #ad_admins	done
+delete from ad_admins;
 load data infile '/Users/michael/school/3EHIF/DBI/Garlic/database/mysql/data/ad_admins.csv'
 into table ad_admins
 fields terminated by ','
 lines terminated by '\n'
 ignore 1 rows;
 
-#s_subscriptions	done
+#s_subscriptions
+delete from s_subscriptions;
 load data infile '/Users/michael/school/3EHIF/DBI/Garlic/database/mysql/data/s_subscriptions.csv'
 into table s_subscriptions
 fields terminated by ','
@@ -45,6 +50,7 @@ lines terminated by '\n'
 ignore 1 rows;
 
 #sm_socialmedias
+delete from sm_socialmedias;
 load data infile '/Users/michael/school/3EHIF/DBI/Garlic/database/mysql/data/sm_socialmedias.csv'
 into table sm_socialmedias
 fields terminated by ','
@@ -52,20 +58,23 @@ lines terminated by '\n'
 ignore 1 rows;
 
 #csm_connectedsocialmedias
+delete from csm_connectedsocialmedias;
 load data infile '/Users/michael/school/3EHIF/DBI/Garlic/database/mysql/data/csm_connectedsocialmedias.csv'
 into table csm_connectedsocialmedias
 fields terminated by ','
 lines terminated by '\n'
 ignore 1 rows;
 
-#r_rankings	done
+#r_rankings
+delete from r_rankings;
 load data infile '/Users/michael/school/3EHIF/DBI/Garlic/database/mysql/data/r_rankings.csv'
 into table r_rankings
 fields terminated by ','
 lines terminated by '\n'
 ignore 1 rows;
 
-#a_articles	done
+#a_articles
+delete from a_articles;
 load data infile '/Users/michael/school/3EHIF/DBI/Garlic/database/mysql/data/a_articles.csv'
 into table a_articles
 fields terminated by ','
@@ -74,13 +83,15 @@ lines terminated by '\n'
 ignore 1 rows;
 
 #c_comments
+delete from c_comments;
 load data infile '/Users/michael/school/3EHIF/DBI/Garlic/database/mysql/data/c_comments.csv'
 into table c_comments
 fields terminated by ','
 lines terminated by '\n'
 ignore 1 rows;
 
-#v_votes	done
+#v_votes
+delete from v_votes;
 load data infile '/Users/michael/school/3EHIF/DBI/Garlic/database/mysql/data/v_votes.csv'
 into table v_votes
 fields terminated by ','
