6b3bb6a346681474fce1ff9fced361f7d1084f59
langhaarzombie <mixmax199989@googlemail.com>
2016-05-30 18:26:57 +0200

    Added: Section above articles to give detailed information about the selected clove; ItemsSource Control via CodeBehind and an extra controller class; article list box now changes when the selection of the clove box is changed; information about the currently selected article is displayed on the right; Task for all queries and longer lasting processes

--- program/MySQL_WPF_Garlic/MySQL_WPF_Garlic/MainWindow.xaml.cs
+++ program/MySQL_WPF_Garlic/MySQL_WPF_Garlic/MainWindow.xaml.cs
@@ -25,41 +25,69 @@ namespace MySQL_WPF_Garlic {
 
         garlicEntities db = new garlicEntities();
 
+        MainWindowController controller;
+
         public MainWindow () {
+            this.controller = new MainWindowController(ref db);
             InitializeComponent();
         }
 
-        private void list_Loaded (object sender, RoutedEventArgs e) {
+        private async void list_Loaded (object sender, RoutedEventArgs e) {
             try {
-                list.ItemsSource = (from c in db.c_clove
-                                    select c).Distinct().ToList();
+                list.IsEnabled = false;
+                articles.IsEnabled = false;
+                list.ItemsSource = await controller.Task_list_ItemsSource();
+                list.DisplayMemberPath = "ci_cloveName";
+                list.IsEnabled = true;
+                articles.IsEnabled = true;
             } catch (Exception) {
+                list.IsEnabled = true;
+                articles.IsEnabled = true;
                 MessageBox.Show("An error occured while trying to connect to the database. Please try again.");
                 throw;
             }
         }
 
-        private int selectedClove;
-        public int SelectedClove {
-            get {
-                return selectedClove;
-            }
-            set {
-                selectedClove = value;
-                PropertyChanged(this, new PropertyChangedEventArgs("articles_Loaded"));
+        private async void list_SelectionChanged (object sender, SelectionChangedEventArgs e) {
+            var selecteditem = (vcloveinfo)list.SelectedItem;
+            list.IsEnabled = false;
+            articles.IsEnabled = false;
+            articles.ItemsSource = await controller.Task_articles_ItemsSource(selecteditem);
+            articles.DisplayMemberPath = "a_title";
+            list.IsEnabled = true;
+            articles.IsEnabled = true;
+            builderCloveInfo(selecteditem.ci_articles, selecteditem.ci_subscribers, selecteditem.ci_admins);
+            builderArticleInfo(null);
+        }
+
+        private void builderCloveInfo (long? numberOfArticles, long? numberOfSubscribers, long? numberOfAdmins) {
+            articlestitle.Text = "Clove Information" + "\n" + "Articles: " + numberOfArticles + "\n" + "Subs: " + numberOfSubscribers + "\n" + "Admins: " + numberOfAdmins;
+        }
+
+        private void articles_SelectionChanged (object sender, SelectionChangedEventArgs e) {
+            var selecteditem = (a_articles)articles.SelectedItem;
+            builderArticleInfo(selecteditem);
+        }
+
+        private async void builderArticleInfo (a_articles article) {
+            if(article == null) {
+                articleid.Text = "-"; articledate.Text = "-"; articleauthor.Text = "-"; articletitle.Text = "-"; articlecontent.Text = "-";
+            } else {
+                //TODO change the view in MySQL so there is only the second view used
+                list.IsEnabled = false;
+                articles.IsEnabled = false;
+                var info = await controller.Task_info_query(article);
+                list.IsEnabled = true;
+                articles.IsEnabled = true;
+                articleid.Text = info.p_id.ToString();
+                articledate.Text = article.p_posts.p_date.ToString();
+                articleauthor.Text = article.p_posts.p_u_username;
+                articletitle.Text = article.a_title.Length > 20 ? article.a_title.Substring(0, 20) + " ..." : article.a_title;
+                articlecontent.Text = article.p_posts.p_content.Substring(0, 20)+" ...";
             }
         }
 
-        private void articles_Loaded (object sender, RoutedEventArgs e) {
-            try {
-                articles.ItemsSource = (from a in db.a_articles
-                                        where a.c_clove.c_id == this.selectedClove
-                                        select a).Distinct().ToList();
-                var i = articles.ItemsSource;
-            } catch (Exception) {
-                MessageBox.Show("An error occured while loading the articles. Please try again");
-                throw;
-            }
-        }
+        
+
     }
 }
---
+++ program/MySQL_WPF_Garlic/MySQL_WPF_Garlic/MainWindowController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySQL_WPF_Garlic {
+    class MainWindowController {
+
+        public garlicEntities db { get; set; }
+
+        public MainWindowController (ref garlicEntities database) {
+            this.db = database;
+        }
+
+        public Task<List<vcloveinfo>> Task_list_ItemsSource () {
+            return Task<List<vcloveinfo>>.Run(() => {
+                return (from ci in db.vcloveinfoes
+                        select ci).Distinct().ToList();
+            });
+        }
+
+        public Task<List<a_articles>> Task_articles_ItemsSource (vcloveinfo selecteditem) {
+            return Task<List<a_articles>>.Run(() => {
+                return (from a in db.a_articles
+                        where a.a_c_clove == selecteditem.ci_cloveID
+                        select a).Distinct().ToList();
+            });
+        }
+
+        public Task<vpostinfo> Task_info_query (a_articles article) {
+            return Task<vpostinfo>.Run(() => {
+                return (from pi in db.vpostinfoes
+                        where pi.p_id == article.a_p_id
+                        select pi).Distinct().ToList().First();
+            });
+        }
+
+    }
+}
